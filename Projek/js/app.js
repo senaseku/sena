968bb15aa4ec6782f554141326f82630f0b0ab85
senaseku <senaseku@gmail.com>
2025-04-09 16:50:49 +0700

    Add files via upload

---
+++ Projek/js/app.js
@@ -0,0 +1,44 @@
+    let display = document.getElementById('display');
+    let currentInput = '';
+
+    function appendNumber(number) {
+      if (currentInput === '0' && number !== '.') {
+        currentInput = number;
+      } else {
+        currentInput += number;
+      }
+      display.innerText = currentInput;
+    }
+
+    function clearDisplay() {
+        currentInput = '0';
+        display.innerText = currentInput;
+    }
+
+    function deleteNumber () {
+      currentInput = currentInput.slice(0, -1);
+      if (currentInput === '') {
+        currentInput = '0';
+      }
+      display.innerText = currentInput;
+    }
+
+    function calculate() {
+    try {
+      if (currentInput === Infinity || isNaN(currentInput)) {
+        throw new Error('Invalid calculation');
+    }
+        currentInput = eval(currentInput);
+    } catch (e) {
+        showAlert('Hasil tidak Ditemukan');
+    }
+    display.innerText = currentInput;
+    }
+    function showAlert(message) {
+        const alertBox = document.getElementById('alert');
+        alertBox.innerText = message;
+        alertBox.classList.add('show');
+        setTimeout(() => {
+            alertBox.classList.remove('show');
+        }, 3000);
+    }
